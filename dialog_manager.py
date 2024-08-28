7a936cdd7fb3119d1f140375cf1cb48cb90d5d16
HuBohy <bohyhugo@gmail.com>
2024-08-28 15:14:27 +0100

    add dialog manager + dialog manager example with RAG

---
+++ dialog_manager.py
@@ -0,0 +1,36 @@
+from s2s.baseHandler import BaseHandler
+import logging
+
+logger = logging.getLogger(__name__)
+
+class DialogManagerHandler(BaseHandler):
+    def setup(
+            self, 
+            device="cpu",
+            rag=None,
+            prompt_creator=None
+    ):
+        self.device = device
+        self.choice = {"question": "answer", "prompt": "chill dude..."}
+        self.rag=rag
+        self.prompt_creator=prompt_creator
+        self.warmup()
+
+    def warmup(self):
+        logger.info(f"Warming up {self.__class__.__name__}")
+        _ = self.choice["question"]
+        
+    def process(self, prompt):
+        prompt = prompt.decode("utf-8")
+        text_type = "question" if prompt.endswith("?") else "prompt"
+        if text_type == "question":
+            answer = self.rag.retrieve_answer(
+                prompt
+            )
+
+        elif text_type == "prompt":
+            # request spontaneous answer
+            answer = "chill dude..."
+            pass
+
+        yield answer
