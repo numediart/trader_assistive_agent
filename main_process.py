7bae8087edf8afec4559c25eeb4827a12ba3dd43
HuBohy <bohyhugo@gmail.com>
2024-09-04 17:17:34 +0100

    add TTS

--- main_process.py
+++ main_process.py
@@ -12,6 +12,7 @@ from audio_streamer import LocalAudioStreamer
 from VAD.vad import VAD
 from ASR.lightning_whisper_mlx import LightningWhisperASR
 from dialog_manager import DialogManagerHandler
+from TTS.melo import MeloTTS
 from rag_example import RAG
 
 
@@ -24,16 +25,23 @@ local_audio_streamer = LocalAudioStreamer(input_queue=rcv_audio_chuncks_queue)
 vad = VAD(should_listen)
 asr = LightningWhisperASR(device="mps")
 dm = DialogManagerHandler(rag=RAG)
+tts = MeloTTS(should_listen)
 
 
 thread = threading.Thread(target=local_audio_streamer.start)
 thread.start()
 print("started recording")
 while True:
+    vad.should_listen = should_listen
     mic_data = local_audio_streamer.input_queue.get()
     spoken_prompt_queue = vad.process(mic_data)
-    if spoken_prompt_queue is not None:
+    should_listen = vad.should_listen
+    if spoken_prompt_queue is not None and not should_listen.is_set():
         prompt = asr.process(spoken_prompt_queue)
         
         # do Dialog Manager
         dm_output = dm.process(prompt)
+
+        audio_output = tts.process(dm_output)
+        should_listen = tts.should_listen
+        
