7fff25feba71d93cc6ab8cd5ef3e7b79b291ed91
HuBohy <bohyhugo@gmail.com>
2024-09-04 11:05:13 +0100

    add LocalAudioStreamer

---
+++ audio_streamer.py
@@ -0,0 +1,38 @@
+import threading
+import sounddevice as sd
+import numpy as np
+
+import time
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class LocalAudioStreamer:
+    def __init__(
+        self,
+        input_queue,
+        list_play_chunk_size=512,
+    ):
+        self.list_play_chunk_size = list_play_chunk_size
+
+        self.stop_event = threading.Event()
+        self.input_queue = input_queue
+
+    def run(self):
+        def callback(indata, frames, time, status):
+            self.input_queue.put(indata.copy())
+
+        logger.debug("Available devices:")
+        logger.debug(sd.query_devices())
+        with sd.InputStream(
+            samplerate=16000,
+            dtype="int16",
+            channels=1,
+            callback=callback,
+            blocksize=self.list_play_chunk_size,
+        ):
+            logger.info("Starting local audio stream")
+            while not self.stop_event.is_set():
+                time.sleep(0.001)
+            print("Stopping recording")
--- main_process.py
+++ main_process.py
@@ -1,15 +1,32 @@
 # RECEIVE DATA FROM QUEUING SYSTEM
-import threading
 
 # RULES TO DECIDE THE ACTION OF THE AGENT
 # 1) RESPOND TO FIAANCE/INVESTING RELATED QUESTIONS => CALL RAG SYSTEM
 # 2) SPONTANOUS COMMUNICATION WITH USER WHEN SPECIFIC STATES DETECTED (STRESS, HIGH RISK TRANSACTION REQUEST)
 # => BIOSIG INTO PROMPT
 # 3) FILL UP A SURVEY WITH THE AVATAR
+from queue import Queue
+import threading
+
+from audio_streamer import LocalAudioStreamer
 from VAD.vad import VAD
 
 should_listen = threading.Event()
 should_listen.set()
 
+rcv_audio_chuncks_queue = Queue()
+spoken_prompt_queue = Queue()
 
+local_audio_streamer = LocalAudioStreamer(input_queue=rcv_audio_chuncks_queue)
 vad = VAD(should_listen)
+
+thread = threading.Thread(target=local_audio_streamer.run)
+thread.start()
+print("started recording")
+while True:
+    if rcv_audio_chuncks_queue.not_empty:
+        spoken_prompt_queue.put(vad.process(rcv_audio_chuncks_queue.get()))
+
+    if spoken_prompt_queue.not_empty:
+        pass
+        # do ASR
